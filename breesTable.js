2a60f4d760e73d176a3e0e620221cea30aa5b45b
Hans Christian Zoll <christianzoll@yahoo.com>
2022-09-21 11:42:23 -0600

    made functioning makeAnArray function

---
+++ breesTable.js
@@ -0,0 +1,39 @@
+// $0
+// $0.firstChild
+// $0.lastChild
+
+//commensurateRow = 0; // integer = to the number of rows until .Table_TR.Table_TD.lastChild.className=AnchorLink.pl2.innerText="NO";
+
+// noRow = document.querySelector('.AnchorLink pl2').parentElement; // first row where .Table_TR.Table_TD.lastChild.className=AnchorLink.pl2.innerText="NO";
+
+// $0.parentElement.parentElement.parentElement; //when $0 = a.className = 'AnchorLink'
+
+yearsTable = document.querySelectorAll(".Table.Table--align-right.Table--fixed.Table--fixed-left")[0];
+
+// yearsTableBody = document.querySelectorAll(".Table.Table--align-right.Table--fixed.Table--fixed-left")[0].lastElementChild
+
+
+// anchorLink = ((yearsTable || {}).)
+
+
+<table class="Table Table--align-right…fixed Table--fixed-left"
+
+children: HTMLCollection { 0: colgroup.Table__Colgroup, 1: thead.Table__header-group.Table__THEAD
+
+
+2: <tbody class="Table__TBODY"
+
+childNodes: NodeList(21) [ tr.Table__TR.Table__TR--sm.Table__even, tr.Table__TR.Table__TR--sm.Table__even, tr.Table__TR.Table__TR--sm.Table__even, … ]
+
+5: <tr class="Table__TR Table__TR--sm Table__even" 
+
+attributes: NamedNodeMap(3) [ class="Table__TR Table__TR--sm Table__even", data-idx="5", style="height: auto;" ]
+
+cells: HTMLCollection {
+1: <td class="Table__TD">
+  
+lastChild: <div class="flex flex-row items-center">
+  
+children: HTMLCollection {
+  
+1: <a class="AnchorLink pl2"
---
+++ helper.js
@@ -0,0 +1,15 @@
+// SIBLING TABLES' CONTAINER
+const entireTable = document.querySelector('.flex');
+
+//SELECT SIBLING TABLES
+const yearColumns = document.querySelector('.Table.Table--align-right.Table--fixed.Table--fixed-left');
+const dataTable = document.querySelector('.Table__ScrollerWrapper.relative.overflow-hidden');
+
+// HELPER FUNCTION TO CONVERT HTMLCOLLECTION to fully operational Array
+const makeAnArray = function(htmlCollection) {
+    return Array.from(htmlCollection)
+    console.log(makeAnArray(htmlCollection));
+};
+
+
+
