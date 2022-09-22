52e8a1a8237b9e29409f2c5e317707b9e7c37843
Hans Christian Zoll <christianzoll@yahoo.com>
2022-09-22 08:02:11 -0600

    crawl first attempt and target value chain

--- helper.js
+++ helper.js
@@ -7,10 +7,31 @@ const dataTable = document.querySelector('.Table__ScrollerWrapper.relative.overf
 
 // HELPER FUNCTION TO CONVERT HTMLCOLLECTION to fully operational Array
 const makeAnArray = function(htmlCollection) {
-    return Array.from(htmlCollection)
+    return Array.from(htmlCollection);
     console.log(makeAnArray(htmlCollection));
 };
 
-// 
+let crawl = (arr) => arr.forEach((index) => { // initiate 
+
+  let findAnArray = Array.from(index).contains("NO");
+
+  if (findAnArray) {
+    return arr.indexOf(findAnArray.closest('.Table__TR.Table__TR--sm.Table__even')); // return first row NO
+  } else {
+    crawl(index);
+  }
+
+} 
+
+crawl(makeAnArray); // call 
 
 
+// let findNo = return makeAnArray.includes(lastChild.innerHTML = "NO")
+
+// let turnIndexIntoArray = run makeAnArray over each array index
+
+// let run includes() over nesting arrays
+
+// if(findNo == true ) {
+    // return makeAnArray.indexOf(no.parent.parent.parent.parent || no.closest('.Table__TR.Table__TR--sm.Table__even');)
+// }
---
+++ noAddress.js
@@ -0,0 +1,5 @@
+yearColumns = document.querySelectorAll(".Table.Table--align-right.Table--fixed.Table--fixed-left");
+
+yearColumns[0].children[2].children[5].children[1].children[0].children[1].innerText // traverse with HTMLCollection properties and/or Array.from()
+
+yearColumns[0].childNodes[2].childNodes[5].childNodes[1].childNodes[0].childNodes[1].innerText // traverese w/ NodeList properties
