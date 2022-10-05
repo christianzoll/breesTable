15e8a82d5ad012b7f334dfcf8457fbd6afa56219
Hans Christian Zoll <66753884+christianzoll@users.noreply.github.com>
2022-10-05 09:40:47 -0600

    Added comments

--- attributeSelector.js
+++ attributeSelector.js
@@ -1,34 +1,42 @@
 // DEEP SEEDED DESCENDENT INDICATES OBJECT PATH
-fleurDeLis = document.querySelector('img[Title=NO]');
+fleurDeLis = document.querySelector('img[Title=NO]'); // DESCENDENT
 
 // OVERSHOOT TARGET ASCENDENT TO REPELL FROM HIGHER ASCENDENT
-column = Array.from(fleurDeLis.closest('table').rows);
+column = Array.from(fleurDeLis.closest('table').rows); // ANCESTOR AVENUE
 
 // REPELL FROM HIGHER ASCENDENT FOR TARGET ASCENDENT'S VALUE
-saintsRow = column.indexOf(fleurDeLis.closest('tr'));
+saintsRow = column.indexOf(fleurDeLis.closest('tr')); // ASCENDENT 
 
-dataRow = saintsRow;
+dataRow = saintsRow; // COLGROUP ASSOCIATION 
 
-gpTh = document.querySelector('th[Title="Games Played"]');
+// SELECT ASSOCIATED DATA IN SIBLING COLGROUP
+gpTh = document.querySelector('th[Title="Games Played"]'); // DESCENDENT
 
-gpThead = gpTh.closest('thead');
+gpThead = gpTh.closest('thead'); // ANCESTOR
 
-gpTheadChildren = Array.from(gpThead.firstChild.children);
+gpTheadChildren = Array.from(gpThead.firstChild.children); // ANCESTOR AVENUE
 
-gpIndex = gpTheadChildren.indexOf(gpTh);
+gpIndex = gpTheadChildren.indexOf(gpTh); // ASCENDENT 
+
+// SELECT DATA RANGE IN COLGROUP'S SIBLING TBODY
   
+relativeTbody = Array.from(gpThead.nextSibling.children); // PROVIDES ARRAY OF TR ELEMENTS
 
-relativeTbody = Array.from(gpThead.nextSibling.children);
+// ENTER EACH TR AND SELECT DESIRED TD
+relativeTbodyColumn = relativeTbody.map((row) => +row.firstChild.innerText) // firstChild WILL BE REPLACED VARIABLE THAT'S SET TO THE INDEX OF THE DESIRED COLUMN
 
-relativeTbodyColumn = relativeTbody.map((row) => +row.firstChild.innerText)
-
-
+// MAKE AN ARRAY OF THE DESIRED TR TD'S
 saintsRows = relativeTbodyColumn.slice(saintsRow-1, -1)
 
-
+// ADD 'EM ALL UP
 saintsTotals = saintsRows.reduce((previous, current) => previous + current);
 
-/*
+
+
+
+
+/* DIDN'T WORK
+
 // FIND() & FINDINDEX() don't work
 fleurDeLis = document.querySelector('img[Title=NO]');
 
